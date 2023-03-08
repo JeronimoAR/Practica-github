aaa21f110f944599b81f23de4e3816f97e26c4ed
Jeronimo Arias <jeronimoarias1212@gmail.com>
2023-03-08 18:39:30 -0500

    proyecto animales

---
+++ Proyecto_animales/src/proyecto_animales/Proyecto_animales.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_animales;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Proyecto_animales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
