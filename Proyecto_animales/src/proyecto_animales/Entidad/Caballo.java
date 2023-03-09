0bf8d9975c8e52bcaaa48c1dc4325a9231732b79
Jeronimo Arias <jeronimoarias1212@gmail.com>
2023-03-08 19:15:36 -0500

    Puse Caballo

--- Proyecto_animales/src/proyecto_animales/Entidad/Caballo.java
+++ Proyecto_animales/src/proyecto_animales/Entidad/Caballo.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyecto_animales.Entidad;
 
-/**
- *
- * @author Lenovo
- */
 public class Caballo {
+    public int peso;
+    public String color,genero;
+
+    public Caballo() {
+    }
+
+    public Caballo(int peso, String color, String genero) {
+        this.peso = peso;
+        this.color = color;
+        this.genero = genero;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
     
 }
